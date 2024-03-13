ad3564a8633160123469a5d115a42bb16c4080a0
DeltaElement <davidtourdotfuentes@gmail.com>
2024-03-13 23:43:01 +0100

    Smooth scroll + side nav bar

--- app_v2.js
+++ app_v2.js
@@ -3,6 +3,8 @@ const firstScreenButton = document.querySelector(".start-experience button");
 const loaderAnim = document.querySelector(".loader");
 const fade = document.querySelector(".fade-transition");
 const body = document.querySelector("body");
+const navLinks = [...document.querySelectorAll(".navlinks-container > a")]
+const sections = [...document.querySelectorAll("main > section")]
 
 const TLAnim = gsap.timeline();
 
@@ -12,13 +14,16 @@ function reloadPage(){
   location.reload();
 }
 
-function reloadJS(file) {
-  var script = document.createElement("script");
-  script.src = "/" + file + ".js";
-  myWindow = window.open(script.src + "?v=" + Math.random());
-  document.body.appendChild(script);
-  myWindow.close();
- }
+navLinks.forEach(link => link.addEventListener("click", addScrollSmooth))
+
+function addScrollSmooth(e){
+  const linkIndex = navLinks.indexOf(e.target);
+  window.scrollTo({
+    top: sections.map(section => section.offsetTop)[linkIndex],
+    behavior: "smooth"
+  })
+}
+
 
 window.addEventListener('load', () => {
   firstScreenButton.textContent = "Visit Portfolio";
@@ -29,39 +34,12 @@ window.addEventListener('load', () => {
   document.body.style.backgroundImage = "url('ressources/generic/background.jpg')";
 })
 
-
-var allLinks = document.querySelectorAll('a[href]');
-var cbk = function(e) {
- if((e.currentTarget.href === window.location.href)) {
-   e.preventDefault();
-   e.stopPropagation();
- }
-
- if(isAnimating === true){
-  e.preventDefault();
-  e.stopPropagation();
- }
-};
-
-for(var i = 0; i < allLinks.length; i++) {
-  allLinks[i].addEventListener('click', cbk);
-}
-
 function startExperience(){
   TLAnim.from(firstScreen, {display: "flex", opacity: '100%', duration: 1})
   TLAnim.to(firstScreen, {opacity: '0%', ease: "power2.in"})
   .set(firstScreen, {display: "none"})
   main();
   playStartExperience();
-  scroll(0,0);
-}
-function startExperiencePlayground(){
-  TLAnim.from(firstScreen, {display: "flex", opacity: '100%', duration: 1})
-  TLAnim.to(firstScreen, {opacity: '0%', ease: "power2.in", duration: 0})
-  .set(firstScreen, {display: "none"})
-  main();
-  playStartExperience();
-  setParam(0, "MusicVol_param", false);
 }
 
 function toggleAnimationPM01(target, animate){
@@ -70,12 +48,4 @@ function toggleAnimationPM01(target, animate){
   }else{
     document.querySelector('.' + target).classList.remove(target + '-extend');
   }
-}
-
-function delay(n){
-  return new Promise((done) => {
-    setTimeout(()=> {
-      done();
-    }, n)
-  })
 }
