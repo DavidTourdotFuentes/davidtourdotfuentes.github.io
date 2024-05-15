96b06b926b080fde606c4809c5e2c2548cbc5d37
DeltaElement <davidtourdotfuentes@gmail.com>
2024-05-15 21:51:38 +0200

    V2 CLEARED

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const firstScreenButton = document.querySelector(".start-experience button");
 const loaderAnim = document.querySelector(".loader");
 const fade = document.querySelector(".fade-transition");
 const body = document.querySelector("body");
+const navLinks = [...document.querySelectorAll(".navlinks-container > a")]
+const sections = [...document.querySelectorAll("main > section")]
 
 const TLAnim = gsap.timeline();
 
@@ -12,13 +14,16 @@ function reloadPage(){
   location.reload();
 }
 
-function reloadJS(file) {
-  var script = document.createElement("script");
-  script.src = "/" + file + ".js";
-  myWindow = window.open(script.src + "?v=" + Math.random());
-  document.body.appendChild(script);
-  myWindow.close();
- }
+navLinks.forEach(link => link.addEventListener("click", addScrollSmooth))
+
+function addScrollSmooth(e){
+  const linkIndex = navLinks.indexOf(e.target);
+  window.scrollTo({
+    top: sections.map(section => section.offsetTop)[linkIndex],
+    behavior: "smooth"
+  })
+}
+
 
 window.addEventListener('load', () => {
   firstScreenButton.textContent = "Visit Portfolio";
@@ -29,39 +34,12 @@ window.addEventListener('load', () => {
   document.body.style.backgroundImage = "url('ressources/generic/background.jpg')";
 })
 
-
-var allLinks = document.querySelectorAll('a[href]');
-var cbk = function(e) {
- if((e.currentTarget.href === window.location.href)) {
-   e.preventDefault();
-   e.stopPropagation();
- }
-
- if(isAnimating === true){
-  e.preventDefault();
-  e.stopPropagation();
- }
-};
-
-for(var i = 0; i < allLinks.length; i++) {
-  allLinks[i].addEventListener('click', cbk);
-}
-
 function startExperience(){
   TLAnim.from(firstScreen, {display: "flex", opacity: '100%', duration: 1})
   TLAnim.to(firstScreen, {opacity: '0%', ease: "power2.in"})
   .set(firstScreen, {display: "none"})
   main();
   playStartExperience();
-  scroll(0,0);
-}
-function startExperiencePlayground(){
-  TLAnim.from(firstScreen, {display: "flex", opacity: '100%', duration: 1})
-  TLAnim.to(firstScreen, {opacity: '0%', ease: "power2.in", duration: 0})
-  .set(firstScreen, {display: "none"})
-  main();
-  playStartExperience();
-  setParam(0, "MusicVol_param", false);
 }
 
 function toggleAnimationPM01(target, animate){
@@ -71,51 +49,3 @@ function toggleAnimationPM01(target, animate){
     document.querySelector('.' + target).classList.remove(target + '-extend');
   }
 }
-
-function delay(n){
-  return new Promise((done) => {
-    setTimeout(()=> {
-      done();
-    }, n)
-  })
-}
-
-barba.init({
-  transitions: [
-    {
-      async leave(data){
-        isAnimating = true;
-
-        // Sound
-        playSwitchPages();
-
-        // System
-        const done = this.async();
-        TLAnim.to(fade, {display: "block", opacity: '100%', ease: "power2.out", duration: 0.5})
-        //.set(body, {overflow: "hidden"})
-
-        await delay(1500);
-        done();
-
-        let href = data.next.url.path;
-        if(href == "/projects.html"){
-          reloadJS("projects");
-          console.log("Loading projects");
-        }
-        if(data.next.url.path == "/playground.html"){
-          setParam(0, "MusicVol_param", false);
-        }else{
-          setParam(1, "MusicVol_param", false);
-          // Stopper tous les events sons de playground
-          stopAllExamples();
-        }
-      },
-      enter(){
-        TLAnim.from(fade, {display: "block", opacity: '100%', duration: 1})
-        TLAnim.to(fade, {opacity: '0%', ease: "power2.in", duration: 0})
-        .set(fade, {display: "none"}) 
-        isAnimating = false;
-      }
-    }
-  ]
-});
---
+++ old/old_app.js
@@ -0,0 +1,121 @@
+const firstScreen = document.querySelector(".start-experience");
+const firstScreenButton = document.querySelector(".start-experience button");
+const loaderAnim = document.querySelector(".loader");
+const fade = document.querySelector(".fade-transition");
+const body = document.querySelector("body");
+
+const TLAnim = gsap.timeline();
+
+var isAnimating = false;
+
+function reloadPage(){
+  location.reload();
+}
+
+function reloadJS(file) {
+  var script = document.createElement("script");
+  script.src = "/" + file + ".js";
+  myWindow = window.open(script.src + "?v=" + Math.random());
+  document.body.appendChild(script);
+  myWindow.close();
+ }
+
+window.addEventListener('load', () => {
+  firstScreenButton.textContent = "Visit Portfolio";
+  firstScreenButton.classList.add('loaded');
+  loaderAnim.classList.add('loaded');
+  firstScreenButton.disabled = false;
+
+  document.body.style.backgroundImage = "url('ressources/generic/background.jpg')";
+})
+
+
+var allLinks = document.querySelectorAll('a[href]');
+var cbk = function(e) {
+ if((e.currentTarget.href === window.location.href)) {
+   e.preventDefault();
+   e.stopPropagation();
+ }
+
+ if(isAnimating === true){
+  e.preventDefault();
+  e.stopPropagation();
+ }
+};
+
+for(var i = 0; i < allLinks.length; i++) {
+  allLinks[i].addEventListener('click', cbk);
+}
+
+function startExperience(){
+  TLAnim.from(firstScreen, {display: "flex", opacity: '100%', duration: 1})
+  TLAnim.to(firstScreen, {opacity: '0%', ease: "power2.in"})
+  .set(firstScreen, {display: "none"})
+  main();
+  playStartExperience();
+  scroll(0,0);
+}
+function startExperiencePlayground(){
+  TLAnim.from(firstScreen, {display: "flex", opacity: '100%', duration: 1})
+  TLAnim.to(firstScreen, {opacity: '0%', ease: "power2.in", duration: 0})
+  .set(firstScreen, {display: "none"})
+  main();
+  playStartExperience();
+  setParam(0, "MusicVol_param", false);
+}
+
+function toggleAnimationPM01(target, animate){
+  if(animate){
+    document.querySelector('.' + target).classList.add(target + '-extend');
+  }else{
+    document.querySelector('.' + target).classList.remove(target + '-extend');
+  }
+}
+
+function delay(n){
+  return new Promise((done) => {
+    setTimeout(()=> {
+      done();
+    }, n)
+  })
+}
+
+barba.init({
+  transitions: [
+    {
+      async leave(data){
+        isAnimating = true;
+
+        // Sound
+        playSwitchPages();
+
+        // System
+        const done = this.async();
+        TLAnim.to(fade, {display: "block", opacity: '100%', ease: "power2.out", duration: 0.5})
+        //.set(body, {overflow: "hidden"})
+
+        await delay(1500);
+        done();
+
+        let href = data.next.url.path;
+        if(href == "/projects.html"){
+          reloadJS("projects");
+          console.log("Loading projects");
+        }
+        if(data.next.url.path == "/playground.html"){
+          setParam(0, "MusicVol_param", false);
+        }else{
+          setParam(1, "MusicVol_param", false);
+          // Stopper tous les events sons de playground
+          stopAllExamples();
+        }
+      },
+      enter(){
+        TLAnim.from(fade, {display: "block", opacity: '100%', duration: 1})
+        TLAnim.to(fade, {opacity: '0%', ease: "power2.in", duration: 0})
+        .set(fade, {display: "none"}) 
+        isAnimating = false;
+      }
+    }
+  ]
+});
